62f36f238047732460464ed9761d5b47f59bcbd6
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-12-19 15:54:22 -0800

    Files generated by PkgTemplates

---
+++ src/CoDa.jl
@@ -0,0 +1,6 @@
+__precompile__()
+module CoDa
+
+# Package code goes here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,7 @@
+using CoDa
+using Base.Test
+
+@testset "CoDa.jl" begin
+    # Write your own tests here.
+    @test 1 == 2
+end
