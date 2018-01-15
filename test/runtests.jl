7b8fada94d398f9262417728bc9645ac84e24fed
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-01-15 13:01:05 -0800

    Add first basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using CoDa
 using Base.Test
 
 @testset "CoDa.jl" begin
-  # TODO
+  @testset "Composition" begin
+    c₁ = Composition(1,2,3)
+    c₂ = Composition(10,20,30)
+    @test c₁ == c₂
+    @test c₁ - c₂ == Composition(1,1,1)
+  end
 end
