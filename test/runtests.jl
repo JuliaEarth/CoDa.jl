e61901eb20b754406d1ec251248a608cf5bf4f68
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-01-15 20:41:53 -0800

    Increase test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,12 @@ using Base.Test
 
 @testset "CoDa.jl" begin
   @testset "Composition" begin
+    c₀ = Composition(1,1,1)
     c₁ = Composition(1,2,3)
     c₂ = Composition(10,20,30)
     @test c₁ == c₂
-    @test c₁ - c₂ == Composition(1,1,1)
+    @test c₁ - c₂ == c₀
+    @test norm(c₀) ≈ 0.
+    @test isapprox(distance(c₁, c₁), 0., atol=1e-6)
   end
 end
