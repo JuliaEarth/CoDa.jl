81ae9fbfe5ee45c3369f90fe150b1e0bc0ae38c4
mralbu <marceloralbuquerque@gmail.com>
2020-09-12 22:56:28 -0300

    Add ilr transform

--- src/transforms.jl
+++ src/transforms.jl
@@ -40,8 +40,23 @@ clrinv(x) = Composition(𝓒(exp.(x)))
 
 Isometric log-ratio transformation of composition `c`.
 """
-function ilr(c::Composition)
-  # TODO
+function ilr(c::Composition{D,SYMS}) where {D,SYMS}
+  log_parts = log.(c.parts .+ eps())
+  x = @MVector zeros(D-1)
+  for i in 1:D-1
+    s = 0.
+    sqrt_denom = 1/sqrt(i*(i+1))
+    for j in 1:i+1
+      if j < i+1
+        s += - sqrt_denom * log_parts[j]
+      elseif j==i+1
+        s += i * sqrt_denom * log_parts[j]
+      end
+    end
+    x[i] = s
+  end
+
+  SVector(x)
 end
 
 """
@@ -49,6 +64,18 @@ end
 
 Inverse ilr for coordinates `x`.
 """
-function ilrinv(x)
-  # TODO
+function ilrinv(x::SVector{D,T}) where {D,T<:Real}
+  z = @MVector zeros(D+1)
+  for i in 1:D+1
+    s = 0.
+    for j in 1:D
+      if i < j+1
+        s += - 1/sqrt(j*(j+1)) * x[j]
+      elseif i==j+1
+        s += j * 1/sqrt(j*(j+1)) * x[j]
+      end
+    end
+    z[i] = exp(s)
+  end
+  Composition(𝓒(SVector(z)))
 end
--- test/transforms.jl
+++ test/transforms.jl
@@ -2,4 +2,5 @@
   c = Composition(1,2,3)
   @test alrinv(alr(c)) == c
   @test clrinv(clr(c)) == c
+  @test ilrinv(ilr(c)) == c
 end
