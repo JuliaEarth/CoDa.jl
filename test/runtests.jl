5425903c8236f428f400a51f2e981bc33af35920
mralbu <marceloralbuquerque@gmail.com>
2020-10-13 22:38:57 -0300

    Add tests for readcoda and compose functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using CoDa
 using Test
+using DataFrames
+using CSV
+using DataDeps
+using RData
 
 # list of maintainers
 maintainers = ["juliohm"]
@@ -8,6 +12,13 @@ maintainers = ["juliohm"]
 istravis = "TRAVIS" ∈ keys(ENV)
 ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
 datadir = joinpath(@__DIR__,"data")
+mkpath(datadir)
+
+# download and setup data dependencies
+register(DataDep("juraset", "A geochemical dataset from the Swiss Jura",
+      "https://github.com/cran/compositions/raw/master/data/juraset.rda"))
+dataset = RData.load(joinpath(datadep"juraset", "juraset.rda"))
+CSV.write(joinpath(datadir,"juraset.csv"), dataset["juraset"])
 
 # list of tests
 testfiles = [
--- test/utils.jl
+++ test/utils.jl
@@ -1,4 +1,26 @@
 @testset "Utils" begin
-  # TODO: readcoda
-  # TODO: compose
+  @testset "readcoda" begin
+    df = readcoda(joinpath(datadir,"juraset.csv"); codanames=(:Cd, :Cu, :Pb, :Co, :Cr, :Ni, :Zn));
+    @test names(df) == ["X", "Y", "Rock", "Land", "Cd|Cu|Pb|Co|Cr|Ni|Zn"]
+    @test size(df) == (359, 5)
+    @test df[1, "Cd|Cu|Pb|Co|Cr|Ni|Zn"] == Composition(Cd=1.74, Cu=25.72, Pb=77.36, Co=9.32, Cr=38.32, Ni=21.32, Zn=92.56)
+
+    df = readcoda(joinpath(datadir,"juraset.csv"); codanames=("Cd", "Cu", "Pb", "Co", "Cr", "Ni", "Zn"));
+    @test names(df) == ["X", "Y", "Rock", "Land", "Cd|Cu|Pb|Co|Cr|Ni|Zn"]
+    @test size(df) == (359, 5)
+    @test df[1, "Cd|Cu|Pb|Co|Cr|Ni|Zn"] == Composition(Cd=1.74, Cu=25.72, Pb=77.36, Co=9.32, Cr=38.32, Ni=21.32, Zn=92.56)
+  end
+
+  @testset "compose" begin
+    data = DataFrame!(CSV.File(joinpath(datadir,"juraset.csv")))
+    df = compose(data, (:Cd, :Cu, :Pb, :Co, :Cr, :Ni, :Zn))
+    @test names(df) == ["X", "Y", "Rock", "Land", "Cd|Cu|Pb|Co|Cr|Ni|Zn"]
+    @test size(df) == (359, 5)
+    @test df[1, "Cd|Cu|Pb|Co|Cr|Ni|Zn"] == Composition(Cd=1.74, Cu=25.72, Pb=77.36, Co=9.32, Cr=38.32, Ni=21.32, Zn=92.56)
+
+    df = compose(data, ("Cd", "Cu", "Pb", "Co", "Cr", "Ni", "Zn"))
+    @test names(df) == ["X", "Y", "Rock", "Land", "Cd|Cu|Pb|Co|Cr|Ni|Zn"]
+    @test size(df) == (359, 5)
+    @test df[1, "Cd|Cu|Pb|Co|Cr|Ni|Zn"] == Composition(Cd=1.74, Cu=25.72, Pb=77.36, Co=9.32, Cr=38.32, Ni=21.32, Zn=92.56)
+  end
 end
