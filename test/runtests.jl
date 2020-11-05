c27e0afeb6248e9b6ece5120d72d2bba9d54f90f
Marcelo Albuquerque <marceloralbuquerque@gmail.com>
2020-11-05 12:52:39 -0300

    Add tests for readcoda and compose functions (#12)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using CoDa
 using Test
+using DataFrames
+using CSV
+using DataDeps
+using RData
 
 # list of maintainers
 maintainers = ["juliohm"]
@@ -7,7 +11,15 @@ maintainers = ["juliohm"]
 # environment settings
 istravis = "TRAVIS" ∈ keys(ENV)
 ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
+ENV["DATADEPS_ALWAYS_ACCEPT"] = true
 datadir = joinpath(@__DIR__,"data")
+mkpath(datadir)
+
+# download and setup data dependencies
+register(DataDep("juraset", "A geochemical dataset from the Swiss Jura",
+      "https://github.com/cran/compositions/raw/master/data/juraset.rda"))
+dataset = RData.load(joinpath(datadep"juraset", "juraset.rda"))
+CSV.write(joinpath(datadir,"juraset.csv"), dataset["juraset"])
 
 # list of tests
 testfiles = [
