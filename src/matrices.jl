fe6b179cb5d4c3c47ad3e59d8609f5d020273742
Rodrigo Loro Schuller <rodrigo.loro@impa.br>
2021-09-14 10:52:56 -0300

    Move user interfaces to the bottom

--- src/matrices.jl
+++ src/matrices.jl
@@ -9,7 +9,6 @@ Square matrix of ones.
 """
 struct JMatrix{T} end
 (J::JMatrix{T})(d::Integer) where {T} = Ones{T}(d, d)
-const J = JMatrix{Bool}()
 (*)(::JMatrix, v::AbstractVector) = Fill(sum(v), length(v))
 (*)(v::Adjoint{<:Any, <:AbstractVector}, J::JMatrix) = (J*v')'
 
@@ -20,7 +19,6 @@ const J = JMatrix{Bool}()
 """
 struct FMatrix{T} end
 (F::FMatrix{T})(d::Integer) where {T} = [Diagonal(fill(one(T), d)) -Ones{T}(d)]
-const F = FMatrix{Int}()
 (*)(::FMatrix{T}, v::AbstractVector) where {T} = Fill(-last(v), length(v)-1) + v[1:length(v)-1]
 (*)(v::Adjoint{<:Any, <:AbstractVector}, ::FMatrix) = [v -sum(v)]
 
@@ -31,7 +29,6 @@ const F = FMatrix{Int}()
 """
 struct GMatrix{T} end
 (G::GMatrix{T})(D::Integer) where {T} = -ones(T, D, D)/D + Diagonal(fill(one(T), D))
-const G = GMatrix{Float64}()
 (*)(::GMatrix, v::AbstractVector) = Fill(-sum(v)/length(v), length(v)) + v
 (*)(v::Adjoint{<:Any, <:AbstractVector}, G::GMatrix) = (G*v')'
 
@@ -42,6 +39,37 @@ const G = GMatrix{Float64}()
 """
 struct HMatrix{T} end
 (H::HMatrix{T})(d::Integer) where {T} = ones(T, d, d) + Diagonal(fill(one(T), d))
-const H = HMatrix{Int}()
 (*)(::HMatrix, v::AbstractVector) = Fill(sum(v), length(v)) + v
 (*)(v::Adjoint{<:Any, <:AbstractVector}, H::HMatrix) = (H*v')'
+
+"""
+    J(d)
+    J
+    F(d)
+    F
+    G(D)
+    G
+    H(d)
+    H
+
+User interfaces for the matrices JMatrix, FMatrix, GMatrix and HMatrix, as
+defined by Aitchison 1986.
+
+## Examples
+
+Ordinary constructors of matrices
+```
+julia> H(3)
+julia> G(3)
+```
+
+Sizeless linear transformations
+```
+julia> v'*F
+julia> G*v
+```
+"""
+const J = JMatrix{Bool}()
+const F = FMatrix{Int}()
+const G = GMatrix{Float64}()
+const H = HMatrix{Int}()
