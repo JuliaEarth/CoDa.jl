829fe6769609766b8f929654c139c58da4f5d08e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2021-09-18 16:35:44 -0300

    Simplify tests with F

--- test/covariances.jl
+++ test/covariances.jl
@@ -9,21 +9,21 @@
 
   # Testing matrix forms transformations (Aitchison 1986, pages 82 and 83)
   ## Test matrix transformation from Τ to Σ (4.25)
-  @test Σ ≈ -0.5 * F(d) * Τ * F(d)'
+  @test Σ ≈ -0.5 * F * Τ * F(d)'
 
   ## Test matrix transformation from Τ to Γ (4.26)
   @test Γ ≈ -0.5 * G(D) * Τ * G(D)
 
   ## Test matrix transformation from Σ to Τ (4.27)
-  Γ₀ = F(d)' * inv(H(d)) * Σ * inv(H(d)) * F(d)
+  Γ₀ = F(d)' * inv(H(d)) * Σ * inv(H(d)) * F
   @test Τ ≈ J * Diagonal(Γ₀) + Diagonal(Γ₀) * J - 2 * Γ₀
 
   ## Test matrix transformation from Σ to Γ (4.28)
-  @test Γ ≈ F(d)' * inv(H(d)) * Σ * inv(H(d)) * F(d)
+  @test Γ ≈ F(d)' * inv(H(d)) * Σ * inv(H(d)) * F
 
   ## Test matrix transformation from Γ to Τ (4.29)
   @test Τ ≈ J * Diagonal(Γ) + Diagonal(Γ) * J - 2 * Γ
 
   ## Test matrix transformation from Γ to Σ (4.30)
-  @test Σ ≈ F(d) * Γ * F(d)'
+  @test Σ ≈ F * Γ * F(d)'
 end
