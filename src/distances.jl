8d931d36e8e8e942adc391c5408d130c23c976e3
José A. S. Silva <32930332+mrr00b00t@users.noreply.github.com>
2021-09-30 13:21:27 -0300

    Update src/distances.jl
    Co-authored-by: Júlio Hoffimann <julio.hoffimann@gmail.com>

--- src/distances.jl
+++ src/distances.jl
@@ -9,10 +9,4 @@ Distance between compositions in Aitchison geometry.
 """
 struct CoDaDistance <: Metric end
 
-"""
-    CoDaDistance()(x,y)
-
-With an object d of CoDaDistance instantiated, CoDaDistance
-simply is d(x,y), where x and y are compositions.
-"""
 (dist::CoDaDistance)(x,y) = distance(x, y)
