f6d1dd5bfa1a9fa233676cd9fa2532718bf5e47a
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2021-12-23 17:43:05 +0100

    Update scitypes.jl (#29)

--- src/scitypes.jl
+++ src/scitypes.jl
@@ -2,9 +2,9 @@
 # Licensed under the MIT License. See LICENCE in the project root.
 # ------------------------------------------------------------------
 
-ST.scitype(::Composition{D}, ::ST.DefaultConvention) where {D} = ST.Compositional{D}
+ST.ST.scitype(::Composition{D}, ::ST.DefaultConvention) where {D} = ST.Compositional{D}
 
-function ST.scitype(::CoDaArray{D}, ::ST.DefaultConvention) where {D}
+function ST.ST.scitype(::CoDaArray{D}, ::ST.DefaultConvention) where {D}
   T = ST.Compositional{D}
   AbstractVector{T}
 end
