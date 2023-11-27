082d4e914444f867ee026e7ec1698669ca61cc6c
Elias Carvalho <eliascarvdev@gmail.com>
2023-11-27 17:50:23 -0300

    Fix the implementation of the AbstractArray interface of CoDaArray

--- test/codaarrays.jl
+++ test/codaarrays.jl
@@ -6,6 +6,9 @@
   @test array[1] == Composition(a=1, b=4, c=7)
   @test array[2] == Composition(a=2, b=5, c=8)
   @test array[3] == Composition(a=3, b=6, c=9)
+  @test array[[1, 2]] == [Composition(a=1, b=4, c=7), Composition(a=2, b=5, c=8)]
+  @test array[[1, 3]] == [Composition(a=1, b=4, c=7), Composition(a=3, b=6, c=9)]
+  @test array[[2, 3]] == [Composition(a=2, b=5, c=8), Composition(a=3, b=6, c=9)]
   @test Tables.istable(array)
   @test Tables.rowaccess(array) == true
   @test Tables.rows(array) == rows
